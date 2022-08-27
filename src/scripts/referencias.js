5a6aa8ac896d9cdd5f400eb8bd453f6a5f937056
PedroMedeiros2 <phsm.dev@gmail.com>
2022-08-26 21:33:59 -0300

    init html

---
+++ src/scripts/referencias.js
@@ -0,0 +1,25 @@
+function gen_tableAll() {
+    fetch('../Table.json')
+        .then(response => response.json())
+        .then(data => {
+            const tblBody = document.querySelector('.tbodyAll');
+            
+            for (let i = 0; i < data.length; i++) {
+                const row = document.createElement('tr');
+
+                var cell = document.createElement('td');
+                var a = document.createElement('a')
+                var cellText = document.createTextNode(data[i].name);
+                a.href = "../pages/tag.html"
+                a.appendChild(cellText)
+                cell.appendChild(a);
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cellText = document.createTextNode(data[i].description);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+
+                tblBody.appendChild(row);
+            }
+        })
+}
