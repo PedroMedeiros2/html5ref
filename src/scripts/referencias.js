9be4975f0afc460e035e63ed18198291bf33b9c0
PedroMedeiros2 <phsm.dev@gmail.com>
2022-08-31 22:30:34 -0300

    table test

--- src/scripts/referencias.js
+++ src/scripts/referencias.js
@@ -8,13 +8,15 @@ function gen_tableAll() {
                 const row = document.createElement('tr');
 
                 var cell = document.createElement('td');
+                cell.classList.add('tdName')
                 var a = document.createElement('a')
                 var cellText = document.createTextNode(data[i].name);
                 a.href = "../pages/tag.html"
                 a.appendChild(cellText)
                 cell.appendChild(a);
                 row.appendChild(cell);
-                cell = document.createElement('td');
+                cell = document.createElement('td')
+                cell.classList.add('tdDescription')
                 cellText = document.createTextNode(data[i].description);
                 cell.appendChild(cellText);
                 row.appendChild(cell);
